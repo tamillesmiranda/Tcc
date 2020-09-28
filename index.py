207d06e71826dc1c5e62417400eeff5df0f91034
tamillesmiranda <48859615+tamillesmiranda@users.noreply.github.com>
2020-09-28 20:48:36 -0300

    Update index.py

--- index.py
+++ index.py
@@ -9,29 +9,38 @@ window.title("Sistema")
 window.geometry("600x300")
 window.configure(background="white")
 window.resizable(width=False,height=False)
+window.attributes("-alpha",0.7)
 
-#logo = PhotoImage(file="icons/tcc.png")
+logo = PhotoImage(file="icons/tcc.png")
 
-LeftFrame = Frame(window, width=200, height=300, bg= "#D5EDF2", relief="raise")
+LeftFrame = Frame(window, width=200, height=300, bg= "#000000", relief="raise")
 LeftFrame.pack(side=LEFT)
 
-RightFrame = Frame(window, width=395, height=300, bg= "#D5EDF2", relief="raise")
+RightFrame = Frame(window, width=395, height=300, bg= "#000000", relief="raise")
 RightFrame.pack(side=RIGHT)
 
-logoLabel = Label(LeftFrame, bg="#D5EDF2")
+logoLabel = Label(LeftFrame,Image=logo, bg="#000000")
 logoLabel.place(x=50, y=100)
 
-UserLabel = Label(RightFrame, text = "Usuário:", font = ("Century Gothic", 20), bg = "#D5EDF2", fg = "black")
+UserLabel = Label(RightFrame, text = "Usuário:", font = ("Century Gothic", 20), bg = "#000000", fg = "white")
 UserLabel.place(x=5, y=100)
 
 UserEntry = ttk.Entry(RightFrame, width=30)
 UserEntry.place(x=150, y=110)
 
-PassLabel = Label(RightFrame, text = "Senha:", font = ("Century Gothic", 20), bg = "#D5EDF2", fg = "black")
+PassLabel = Label(RightFrame, text = "Senha:", font = ("Century Gothic", 20), bg = "#000000", fg = "white")
 PassLabel.place(x=5, y=150)
 
 PassEntry = ttk.Entry(RightFrame, width=30)
 PassEntry.place(x=150, y=160)
+
+#buttons
+
+LoginButton = ttk.Button(RightFrame, text="Login", width=30)
+LoginButton.place(x=100, y= 225)
+
+RegisterButton = ttk.Button(RightFrame, text="Cadastrar", width=20)
+RegisterButton.place(x=125, y= 260)
 
 
 
